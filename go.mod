f279dbd9e9c37edb1441caa04ad29c558ae2a797
chedabob <matt@mattmalone.app>
2021-10-04 20:51:08 +0100

    feat: Add support for Brewfile https://github.com/bitrise-steplib/steps-brew-install/issues/11
    Reworked test workflow to not created nested _tmp directories
    Added _tmp to .gitignore file
    Changed `packages` step parameter to optional to allow usage of Brewfile
    Added use_brewfile and brewfile_path step parameters
    Added function and tests for generating a `brew bundle` command
    Added bundle and bundle_path workflows for testing Brewfile support
    Fixed `bitrise run test` not working as there was no go module and the vendor folder needed to be synced

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/bitrise-steplib/steps-brew-install
+
+go 1.17
+
+require (
+	github.com/bitrise-io/go-steputils v0.0.0-20190806143347-f540824d77df
+	github.com/bitrise-io/go-utils v0.0.0-20190814105345-ad86bf775472
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
+	github.com/stretchr/testify v1.4.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
--- main_test.go
+++ main_test.go
@@ -62,3 +62,51 @@ func Test_cmdArgs(t *testing.T) {
 		require.Equal(t, []string{"install", "option_1", "option_2", "go"}, args)
 	}
 }
+
+func Test_brewFileArgs(t *testing.T) {
+	t.Log("Create bundle args with no options or verbose option")
+	{
+		options := ""
+		verboseLog := false
+
+		args := brewFileArgs(options, verboseLog, "")
+		require.Equal(t, []string{"bundle"}, args)
+	}
+
+	t.Log("Create bundle args with verbose log")
+	{
+		options := ""
+		verboseLog := true
+
+		args := brewFileArgs(options, verboseLog, "")
+		require.Equal(t, []string{"bundle", "-v"}, args)
+	}
+
+	t.Log("Create bundle args with verbose option")
+	{
+		options := "--verbose"
+		verboseLog := true
+
+		args := brewFileArgs(options, verboseLog, "")
+		require.Equal(t, []string{"bundle", "--verbose"}, args)
+	}
+
+	t.Log("Create bundle args with options")
+	{
+		options := "option_1 option_2"
+		verboseLog := false
+
+		args := brewFileArgs(options, verboseLog, "")
+		require.Equal(t, []string{"bundle", "option_1", "option_2"}, args)
+	}
+
+	t.Log("Create bundle args with brewfile location")
+	{
+		options := ""
+		verboseLog := false
+		path := "path/to/Brewfile"
+
+		args := brewFileArgs(options, verboseLog, path)
+		require.Equal(t, []string{"bundle", "--file", "path/to/Brewfile"}, args)
+	}
+}
