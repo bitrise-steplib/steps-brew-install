c9a0f6ae71c7a8fd011d34240cf6d958067c51f1
Olivér Falvai <ofalvai@gmail.com>
2024-04-23 13:46:56 +0200

    Print important config values

---
+++ brew_config.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/bitrise-io/go-utils/v2/command"
+	"github.com/bitrise-io/go-utils/v2/env"
+	"github.com/bitrise-io/go-utils/v2/log"
+	"github.com/bitrise-io/go-utils/v2/log/colorstring"
+)
+
+type brewConfigPrinter struct {
+	cmdFactory command.Factory
+	envRepo    env.Repository
+	logger log.Logger
+}
+
+func (p brewConfigPrinter) printBrewConfig() {
+	p.logger.Infof("Homebrew configuration:")
+	
+	versionStr, err := p.cmdFactory.Create("brew", []string{"--version"}, nil).RunAndReturnTrimmedCombinedOutput()
+	if err != nil {
+		p.logger.Warnf("Homebrew version: %s", err)
+	} else {
+		p.logger.Printf("Version: %s", colorstring.Cyan(versionStr))
+	}
+
+	for _, env := range []string{
+		"HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK",
+		"HOMEBREW_NO_INSTALL_CLEANUP",
+		"HOMEBREW_NO_AUTO_UPDATE",
+		"HOMEBREW_CORE_GIT_REMOTE",
+		"HOMEBREW_NO_INSTALL_FROM_API",
+	} {
+		p.printEnv(env)
+	}
+
+	p.logger.Printf("Default values are documented at https://docs.brew.sh/Manpage#environment")
+}
+
+func (p brewConfigPrinter) printEnv(env string) {
+	value := p.envRepo.Get(env)
+	if value == "" {
+		value = "<unset>"
+	}
+
+	p.logger.Printf("%s: %s", env, colorstring.Cyan(value))
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/bitrise-io/go-steputils v1.0.6
 	github.com/bitrise-io/go-utils v1.0.13
+	github.com/bitrise-io/go-utils/v2 v2.0.0-alpha.21
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
 	github.com/stretchr/testify v1.9.0
 )
